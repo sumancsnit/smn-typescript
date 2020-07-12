d163dd88e64794bfdedc110e80ba58c451447a67
Suman Kumar <sumancsnit@gmail.com>
2020-07-12 23:28:11 +0530

    custom types

--- app.ts
+++ app.ts
@@ -1,7 +1,19 @@
-const add = (n1: number, n2: number) => n1 + n2;
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+  //   throw new Error('Incorrect input!');
+  // }
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
+}
 
-add(4, 7);
+let number1: number;
+number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+let resultPhrase = 'Result is: ';
 
-let name7: boolean;
-
-name7 = true
+add(number1, number2, printResult, resultPhrase);
